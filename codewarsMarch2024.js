44212989fd0911e7cd9600a6eeef09826b93017f
longhairshortcode <116141725+longhairshortcode@users.noreply.github.com>
2024-04-20 20:37:49 -0700

    Update codewarsMarch2024.js

--- codewarsMarch2024.js
+++ codewarsMarch2024.js
@@ -1,3 +1,11 @@
+//ANSWER******
+function sum (numbers) {
+    if (numbers.length === 0){
+      return 0
+    }else
+  return numbers.reduce((a, b) => a + b)
+};
+
 //Tuesday March 26, 2024
 
 //TASK*******
